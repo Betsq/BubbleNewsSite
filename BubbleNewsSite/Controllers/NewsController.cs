893b9229938ab4f4648ff21ffaf26395ca53c580
Betsq <betsq9@gmail.com>
2021-01-30 18:29:58 +0200

    -View news and create news

---
+++ BubbleNewsSite/Controllers/NewsController.cs
@@ -0,0 +1,37 @@
+﻿using BubbleNewsSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BubbleNewsSite.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly NewsContext db;
+        public NewsController(NewsContext newsContext)
+        {
+            db = newsContext;
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        #region Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(News news)
+        {
+            db.News.Add(news);
+            await db.SaveChangesAsync();
+            return RedirectToAction("News");
+        }
+        #endregion
+    }
+}
--- BubbleNewsSite/Models/News.cs
+++ BubbleNewsSite/Models/News.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace BubbleNewsSite.Models
     public class News
     {
         public int Id { get; set; }
+        [Display(Name = "Article")]
         public string Article { get; set; }
+        [Display(Name = "Desription")]
         public string Description { get; set; }
+        [Display(Name = "NewsType")]
         public string NewsType { get; set; }
+        [Display(Name = "Img")]
         public string Img { get; set; } //Temporarily by reference  
     }
 }
