a484fdbcf7f6c3f9f1944dc409e38ad356c14b5b
Betsq <betsq9@gmail.com>
2021-01-30 22:33:22 +0200

    -Edit -Details -Delete

--- BubbleNewsSite/Controllers/NewsController.cs
+++ BubbleNewsSite/Controllers/NewsController.cs
@@ -1,5 +1,6 @@
 ﻿using BubbleNewsSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,11 @@ namespace BubbleNewsSite.Controllers
             return View();
         }
 
+        public async Task<IActionResult> NewsCDDE()
+        {
+            return View(await db.News.ToListAsync());
+        }
+
         #region Create
         [HttpGet]
         public IActionResult Create()
@@ -31,6 +37,68 @@ namespace BubbleNewsSite.Controllers
             db.News.Add(news);
             await db.SaveChangesAsync();
             return RedirectToAction("News");
+        }
+        #endregion
+
+        #region Edit
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                News news = await db.News.FirstOrDefaultAsync(n => n.Id == id);
+                if (news != null)
+                    return View(); 
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(News news)
+        {
+            db.News.Update(news);
+            await db.SaveChangesAsync();
+            return RedirectToAction("News");
+        }
+        #endregion
+
+        #region Details
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                News news = await db.News.FirstOrDefaultAsync(n => n.Id == id);
+                if (news != null)
+                    return View(news);
+            }
+            return NotFound();
+        }
+        #endregion
+
+        #region Delete
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                News news = await db.News.FirstOrDefaultAsync(p => p.Id == id);
+                if (news != null)
+                    return View(news);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            News news = await db.News.FirstOrDefaultAsync(p => p.Id == id);
+            if (news != null)
+            {
+                db.News.Remove(news);
+                await db.SaveChangesAsync();
+                return RedirectToAction("News");
+            }
+            return NotFound();
         }
         #endregion
     }
