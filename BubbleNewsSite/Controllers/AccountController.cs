b658f204afa2d87cca3de24931e4d714e0075fba
Betsq <betsq9@gmail.com>
2021-01-31 19:02:53 +0200

    -Login and Logout

--- BubbleNewsSite/Controllers/AccountController.cs
+++ BubbleNewsSite/Controllers/AccountController.cs
@@ -18,6 +18,7 @@ namespace BubbleNewsSite.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+        #region Register
         [HttpGet]
         public IActionResult Register()
         {
@@ -44,5 +45,50 @@ namespace BubbleNewsSite.Controllers
             }
             return View(model);
         }
+        #endregion
+
+        #region Login
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email,
+                    model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect login and (or) password");
+                }
+            }
+            return View(model);
+        }
+        #endregion
+
+        #region Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
     }
 }
